9d29aedc04fed070769e6013b33d74e1ce568473
Djarma <kutri.denisz@gmail.com>
2020-02-25 01:19:39 +0100

    index page done

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import 'jquery';
+import 'slick-carousel';
 
 // scss
 import '../scss/main.scss';
@@ -6,5 +7,14 @@ import '../scss/main.scss';
 
 
 $(document).ready(function() {
-    console.log('it works!!');
+    
+    // slick carousel
+    $('.explore--bg').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="/src/img/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="/src/img/arrow_right.png"></button>',
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        speed: 1500,
+        dots: true
+    });
 });
